e66daafdf44408aa2f1cb6e5ad079357fcd26214
bhoopal <bhoopal10@gmail.com>
2015-10-22 23:28:43 +0530

    service page update

---
+++ service1.php
@@ -0,0 +1,320 @@
+<?php $is_active="service"; include('components/header.php') ?>
+<div class="content service-three">
+  <div class="container">
+
+  <h2>Service</h2>
+  <p class="big grey">Here are the services which we do.</p>
+  <hr>
+
+  <div class="row" id="account-reactivation">
+     <div class="col-md-4 col-sm-4">
+      <!-- Service title with tag -->
+        <h6 class="bold grey">Get Back in Business!</h6>
+        <h3>Account Reactivation</h3>
+        <p>With this mail, we are pleased to introduce you to our Account Reactivation and Management services which can not only help you get your business back on Amazon but which will also ensure that it doesn't get suspended ever again. </p>
+     </div>
+
+     <div class="col-md-8 col-sm-8">
+        <!--  Service #1 -->
+        <!-- Service icon -->
+        <div class="service-icon red">
+          <i class="fa fa-camera"></i>
+        </div>
+
+        <div class="service-content">
+          <!-- Title -->
+          <p><strong>Flexible pricing- </strong>Fixed and variable both options available</p>
+        </div>
+
+        <hr>
+
+        <!-- Service #2 -->
+
+        <div class="service-icon orange">
+          <i class="fa fa-desktop"></i>
+        </div>
+
+        <div class="service-content">
+          <p>No advance payment. <strong>Payment only after the account gets reinstated!</strong></p>
+        </div>
+
+        <hr>   
+
+        <!-- Service #3 -->
+
+        <div class="service-icon lblue">
+          <i class="fa fa-cloud"></i>
+        </div>
+
+        <div class="service-content">
+          <p><strong>TAT (Turnaround Time) = 4-5 days</strong></p>
+        </div>
+        
+        <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p><strong>More than 100 active accounts </strong>being currently managed by us on Amazon</p>
+        </div>
+        <hr>
+
+        <!-- Service #5 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p><strong>95% of the reactivated accounts being managed by us have a 'Good' Account Health now!</strong></p>
+        </div>
+        
+        <br>
+
+
+        <div class="clearfix"></div>
+
+     </div>
+  </div>
+<div class="border"></div>
+  <!--  -->
+  <div class="row" id="listing-cataloguing">
+     <div class="col-md-4 col-sm-4">
+      <!-- Service title with tag -->
+        <h6 class="bold grey">Make your products stand out!</h6>
+        <h3>Listing & Cataloguing</h3>
+        <p>With this mail, we are pleased to offer <b>our professional cataloguing and listing services</b> which can help expand your online product portfolio and <b>enhance your customer experience and sales on all e-Commerce platforms.</b></p>
+       
+     </div>
+
+     <div class="col-md-8 col-sm-8">
+        <!--  Service #1 -->
+        <!-- Service icon -->
+        <div class="service-icon red">
+          <i class="fa fa-camera"></i>
+        </div>
+
+        <div class="service-content">
+          <!-- Title -->
+          <p>Cataloguing at <b>Rs 15-20/- per item</b> only. Variations at just<b> Rs 5/-</b> per item</p>
+        </div>
+
+        <hr>
+
+        <!-- Service #2 -->
+
+        <div class="service-icon orange">
+          <i class="fa fa-desktop"></i>
+        </div>
+
+        <div class="service-content">
+          <p>Listing at <b>Rs 5/- per item (per platform)</b></p>
+        </div>
+
+        <hr>   
+
+        <!-- Service #3 -->
+
+        <div class="service-icon lblue">
+          <i class="fa fa-cloud"></i>
+        </div>
+
+        <div class="service-content">
+          <p>Aenean sodales augue ac lacus hendrerit sed rhoncus erat hendrerit. Vivamus vel ultricies elit.</p>
+        </div>
+        
+        <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p>Volume based discounts</p>
+        </div>
+        <hr>
+
+        <!-- Service #5 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p><b>Over 200 suspended </b>seller accounts reinstated by our PoA experts so far</p>
+        </div>
+        <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p>No advance payment. <b>All payments after the products are live!</b></p>
+        </div>
+        <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p>We won't just share the catalogue excel sheet with you. <b>We'll make your products live and stand out!</b></p>
+        </div>
+        <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p><b>TAT (Turnaround Time) = 2 days</b></p>
+        </div>
+         <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p><b>Experience of listing over 1 lakh items</b></p>
+        </div>
+        
+        <br>
+
+
+        <div class="clearfix"></div>
+
+     </div>
+  </div>
+ <div class="border"></div>
+  <!--  -->
+  <div class="row" id="suspension-prevention">
+     <div class="col-md-4 col-sm-4">
+      <!-- Service title with tag -->
+        <h6 class="bold grey">Keep Calm and Take control!</h6>
+        <h3>Suspension Prevention</h3>
+        <p>With this mail, we are pleased to introduce you to our <b>Suspension Prevention services</b> which can not only help <b>you avoid getting suspended</b> on Amazon but which will also ensure that you are<b> among the best </b>rated sellers in your category.</p>
+        
+     </div>
+
+     <div class="col-md-8 col-sm-8">
+        <!--  Service #1 -->
+        <!-- Service icon -->
+        <div class="service-icon red">
+          <i class="fa fa-camera"></i>
+        </div>
+
+        <div class="service-content">
+          <!-- Title -->
+          <p><b>Having reinstated over 250 suspended</b> seller accounts so far, we know the subject in & out!</p>
+        </div>
+
+        <hr>
+
+        <!-- Service #2 -->
+
+        <div class="service-icon orange">
+          <i class="fa fa-desktop"></i>
+        </div>
+
+        <div class="service-content">
+          <p><b>More than 100 active accounts</b> being currently managed by us on Amazon</p>
+        </div>
+
+        <hr>   
+
+        <!-- Service #3 -->
+
+        <div class="service-icon lblue">
+          <i class="fa fa-cloud"></i>
+        </div>
+
+        <div class="service-content">
+          <p>95% of the reactivated accounts being managed by us have a 'Good' Account Health now!</p>
+        </div>
+        
+        <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p>We'll take care of<b> parameters such as Negative Feedback, A-Z claims etc.</b> that affect your seller performance metrics</p>
+        </div>
+         <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p>Complete customer care support over email and as well as phone- <b>even on Sundays and Holidays!</b></p>
+        </div>
+         <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p>Understand where claims of <b>"counterfeit"</b> and <b>"not as described"</b> really come from and fix your vulnerabilities</p>
+        </div>
+         <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p>We'll <b>track and respond appropriately to Amazon Policy Notifications / Warnings</b></p>
+        </div>
+        <hr>
+
+        <!-- Service #4 -->
+
+        <div class="service-icon purple">
+          <i class="fa fa-user"></i>
+        </div>
+
+        <div class="service-content">
+          <p><b>No advance payment</b></p>
+        </div>
+        
+        <br>
+
+
+        <div class="clearfix"></div>
+
+     </div>
+  </div>
+
+
+<!-- Services Ends -->
+
+</div>
+</div>
+<?php include('components/footer.php') ?>
