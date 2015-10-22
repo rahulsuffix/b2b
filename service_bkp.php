4f48df6ed97d3fe0495960415237e539f6bf6e05
bhoopal <bhoopal10@gmail.com>
2015-10-23 00:29:52 +0530

    service page update

---
+++ service_bkp.php
@@ -0,0 +1,67 @@
+<?php $is_active="service"; include('components/header.php') ?>
+<div class="content">
+	<div class="container">
+		<div class="row" id="account-reactivation">
+			<div class="col-md-12">
+				<h1>Account Reactivation</h1>
+				<p class="grey big">Get Back in Business!</p>
+				<p>
+					With this mail, we are pleased to introduce you to our <strong>Account Reactivation and Management services</strong> which can not only help you get your business back on Amazon but which will also ensure that it doesn't get suspended ever again.
+				</p>
+				<h5>Key features of our Account Reactivation and Management services are as follows:</h5>
+				<ul class="bolds">
+					<li><strong>Flexible pricing- </strong>Fixed and variable both options available</li>
+					<li>No advance payment. <strong>Payment only after the account gets reinstated!</strong></li>
+					<li><strong>TAT (Turnaround Time) = 4-5 days</strong></li>
+					<li><strong>More than 100 active accounts </strong>being currently managed by us on Amazon</li>
+					<li><strong>95% of the reactivated accounts being managed by us have a 'Good' Account Health now!</strong></li>
+				</ul>
+			</div>
+		</div>
+		<hr>
+		<div class="row" id="listing-cataloguing">
+			<div class="col-md-12">
+				<h1>Listing & Cataloguing</h1>
+				<p class="grey big">Make your products stand out!</p>
+				<p>
+					With this mail, we are pleased to offer <b>our professional cataloguing and listing services</b> which can help expand your online product portfolio and <b>enhance your customer experience and sales on all e-Commerce platforms.</b>
+				</p>
+				<h5>Key features of our Cataloguing and Listing services are as follows:</h5>
+				<ul class="bolds">
+					<li>Cataloguing at <b>Rs 15-20/- per item</b> only. Variations at just<b> Rs 5/-</b> per item</li>
+					<li>Listing at <b>Rs 5/- per item (per platform)</b></li>
+					<li>Volume based discounts</li>
+					<li><b>Over 200 suspended </b>seller accounts reinstated by our PoA experts so far</li>
+					<li>No advance payment. <b>All payments after the products are live!</b></li>
+					<li>We won't just share the catalogue excel sheet with you. <b>We'll make your products live and stand out!</b></li>
+					<li><b>TAT (Turnaround Time) = 2 days</b></li>
+					<li><b>Experience of listing over 1 lakh items</b></li>
+				</ul>
+			</div>
+		</div>
+	
+	<hr>
+		<div class="row" id="suspension-prevention">
+			<div class="col-md-12">
+				<h1>Suspension Prevention</h1>
+				<p class="grey big">Keep Calm and Take control!</p>
+				<p>
+					With this mail, we are pleased to introduce you to our <b>Suspension Prevention services</b> which can not only help <b>you avoid getting suspended</b> on Amazon but which will also ensure that you are<b> among the best </b>rated sellers in your category.
+				</p>
+				<strong>Don't Let the Suspension Notice from Amazon be the first signal of trouble.</strong>
+				<h5>Key features of our <b>Suspension Prevention</b> services are as follows: </h5>
+				<ul class="bolds">
+					<li><b>Having reinstated over 250 suspended</b> seller accounts so far, we know the subject in & out!</li>
+					<li><b>More than 100 active accounts</b> being currently managed by us on Amazon</li>
+					<li>95% of the reactivated accounts being managed by us have a 'Good' Account Health now!</li>
+					<li>We'll take care of<b> parameters such as Negative Feedback, A-Z claims etc.</b> that affect your seller performance metrics</li>
+					<li>Complete customer care support over email and as well as phone- <b>even on Sundays and Holidays!</b></li>
+					<li>Understand where claims of <b>"counterfeit"</b> and <b>"not as described"</b> really come from and fix your vulnerabilities</li>
+					<li>We'll <b>track and respond appropriately to Amazon Policy Notifications / Warnings</b></li>
+					<li><b>No advance payment</b></li>
+				</ul>
+			</div>
+		
+	</div>
+</div>
+<?php include('components/footer.php') ?>
